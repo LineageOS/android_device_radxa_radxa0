1e404a46883bdb194f2137f652e95dc9f791ec9c
Nolen Johnson <johnsonnolen@gmail.com>
2021-06-17 00:43:04 -0400

    radxa0: Initial Commit
    Change-Id: I5bbc92935d81897929edf3daefb27a9e09691f60

---
+++ BoardConfig.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+include device/amlogic/g12-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/radxa/radxa0
+
+## Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := radxa0
---
+++ lineage_radxa0.mk
@@ -0,0 +1,21 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common AOSP stuff
+$(call inherit-product, device/google/atv/products/atv_base.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_tv.mk)
+
+# Inherit device configuration
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_BRAND := Radxa
+PRODUCT_DEVICE := radxa0
+PRODUCT_MANUFACTURER := radxa
+PRODUCT_MODEL := Radxa Zero
+PRODUCT_NAME := lineage_radxa0
