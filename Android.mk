0d133b1699af78308f7098441e4c9f634e305e2c
Jan Altensen <info@stricted.net>
2021-07-03 02:50:53 +0200

    radxa0: add aml_upgrade build target
     * will be neded for initial flashing
    
    Change-Id: Ife02f48a7b07421079470e06c05d459556b68930

---
+++ Android.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+ifeq ($(TARGET_DEVICE), radxa0)
+
+LOCAL_PATH := $(call my-dir)
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
+include $(LOCAL_PATH)/factory.mk
+
+endif
