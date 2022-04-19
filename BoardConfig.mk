40536acf6c0ef7c4d7dbdb3db8e111fa0924a2cf
Nolen Johnson <johnsonnolen@gmail.com>
2022-04-19 13:08:56 -0400

    radxa0: Decommonize ro.vendor.platform.support.dolbyvision
    Change-Id: Ie7f134d2b40e8ab38a2b217230f5dc6769a22637

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,6 +20,9 @@ TARGET_BOOTLOADER_BOARD_NAME := radxa0
 ## DTB
 TARGET_DTB_NAME := g12a_s905y2_radxa0
 
+## Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 ## Wi-Fi
 BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
 BOARD_WLAN_DEVICE := bcmdhd
