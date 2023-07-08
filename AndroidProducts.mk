0ff00541ee261e7f7d5a0f1923d8583678fe5eac
Nolen Johnson <johnsonnolen@gmail.com>
2023-07-08 18:28:14 -0400

    radxa0: Allow tablet builds
    Change-Id: Ib400d6c6d87e4d663335f2fb73b47297462561a6

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,14 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_radxa0.mk
+    $(LOCAL_DIR)/lineage_radxa0.mk \
+    $(LOCAL_DIR)/lineage_radxa0_tab.mk
 
 COMMON_LUNCH_CHOICES := \
     lineage_radxa0-eng \
+    lineage_radxa0_tab-eng \
     lineage_radxa0-user \
-    lineage_radxa0-userdebug
+    lineage_radxa0_tab-user \
+    lineage_radxa0-userdebug \
+    lineage_radxa0_tab-userdebug
+
