28d720dce6523ba2c6463f5eea32228e5fdd6c03
Bruno Martins <bgcngm@gmail.com>
2023-10-25 14:57:24 +0100

    radxa0: Move tablet builds support to the proper tree
    Otherwise EPPE is broken since it expects the following path:
    device/*/$(LINEAGE_BUILD)/$(TARGET_PRODUCT).mk
    
    Change-Id: Iadef814f0a3bf4f1948da73b8fe437b10ebba8ed

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,18 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021-2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_radxa0.mk \
-    $(LOCAL_DIR)/lineage_radxa0_tab.mk
+    $(LOCAL_DIR)/lineage_radxa0.mk
 
 COMMON_LUNCH_CHOICES := \
     lineage_radxa0-eng \
-    lineage_radxa0_tab-eng \
     lineage_radxa0-user \
-    lineage_radxa0_tab-user \
-    lineage_radxa0-userdebug \
-    lineage_radxa0_tab-userdebug
-
+    lineage_radxa0-userdebug
