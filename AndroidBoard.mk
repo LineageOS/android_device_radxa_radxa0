94b71ce209323f5d8446bd888c63b4d6625adb40
Aaron Kling <webgeek1234@gmail.com>
2024-06-03 21:32:56 -0500

    radxa0: Add AML install packages to target-files
    To allow the official download portal to offer them
    
    Change-Id: I729c5b95c10527ecdf25b70d9ead23c52ef373ab

---
+++ AndroidBoard.mk
@@ -0,0 +1,23 @@
+# Copyright (C) 2024 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ifeq ($(word 2,$(subst _, ,$(TARGET_PRODUCT))),radxa0)
+BUILT_TARGET_FILES_ZIPROOT := $(call intermediates-dir-for,PACKAGING,target_files)/$(TARGET_PRODUCT)-target_files
+$(BUILT_TARGET_FILES_ZIPROOT).zip: $(BUILT_TARGET_FILES_ZIPROOT)/IMAGES/aml_install_package $(BUILT_TARGET_FILES_ZIPROOT)/IMAGES/aml_install_package.img
+
+$(BUILT_TARGET_FILES_ZIPROOT)/IMAGES/aml_install_package.img: $(BUILT_TARGET_FILES_ZIPROOT).zip.list $(PRODUCT_OUT)/aml_install_package.img
+	@mkdir -p $(dir $@)
+	@cp $(PRODUCT_OUT)/aml_install_package.img $@
+	@echo $@ >> $(BUILT_TARGET_FILES_ZIPROOT).zip.list
+endif
