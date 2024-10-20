e041c1fa0fae714c9c737b3c42fed9d07b9563ce
Bruno Martins <bgcngm@gmail.com>
2024-10-20 11:35:53 +0100

    radxa0: Drop extract setup
    * We used to use this for more, but now there are 0 files.
    * Just a bootloader image that we build out of tree, so
      extraction makes no sense. Just hand create firmware.mk @
      `vendor/amlogic/${DEVICE}/firmware.mk`, store bootloader @
      `endor/amlogic/${DEVICE}/radio/bootloader.img`.
    
    Change-Id: I1e362296e937306f059da6e4e0c240aebc2a7921

---
+++ extract-files.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+module = ExtractUtilsModule(
+    'radxa0',
+    'radxa',
+    skip_main_proprietary_file=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(module, '../amlogic/g12-common', module.vendor)
+    utils.run()
