d98159ddbc9802c897a44ec4b59f930700e4802c
Nolen Johnson <johnsonnolen@gmail.com>
2025-02-07 18:20:01 -0500

    radxa0: Kill Android.mk
    Change-Id: Ib73fc648dd0e0131c065005d622b7322dce509ae

--- build/tasks/factory.mk
+++ build/tasks/factory.mk
@@ -14,9 +14,11 @@
 # limitations under the License.
 #
 
+ifneq ($(filter radxa0 radxa0_car radxa0_tab,$(TARGET_DEVICE)),)
+
 FACTORY_PATH := device/radxa/radxa0/factory
 
-RADIO_IMAGES += \
+RADIO_IMAGES := \
     $(FACTORY_PATH)/bootfiles/bootloader.img \
     $(FACTORY_PATH)/bootfiles/bootloader-console.img \
     $(FACTORY_PATH)/bootfiles/bootloader-recovery.img \
@@ -28,6 +30,11 @@ $(INSTALLED_AML_RADIO_IMAGES): $(RADIO_IMAGES) $(ACP)
         $(shell $(hide) $(ACP) $(f) $(call intermediates-dir-for,PACKAGING,target_files)/$(TARGET_PRODUCT)-target_files/RADIO/$(strip $(notdir $(f)))))
 
 $(INSTALLED_2NDBOOTLOADER_TARGET): $(INSTALLED_AML_RADIO_IMAGES)
+
+INSTALLED_RADIOIMAGE_TARGET += $(FACTORY_PATH)/bootfiles/bootloader.img
+INSTALLED_RADIOIMAGE_TARGET += $(FACTORY_PATH)/bootfiles/bootloader-console.img
+INSTALLED_RADIOIMAGE_TARGET += $(FACTORY_PATH)/bootfiles/bootloader-recovery.img
+INSTALLED_RADIOIMAGE_TARGET += $(FACTORY_PATH)/bootfiles/misc.img
 
 PRODUCT_INSTALL_OUT := $(PRODUCT_OUT)/aml_install
 PRODUCT_UPGRADE_OUT := $(PRODUCT_OUT)/aml_upgrade
@@ -62,8 +69,7 @@ INSTALL_IMAGES := \
     vbmeta.img \
     super.img \
     super_empty.img \
-    logo.img \
-    misc.img
+    logo.img
 
 $(INSTALLED_AML_INSTALL_PACKAGE_TARGET): $(addprefix $(PRODUCT_OUT)/,$(INSTALL_IMAGES)) $(ACP) $(AML_IMAGE_TOOL)
 	$(hide) mkdir -p $(PRODUCT_INSTALL_OUT)
@@ -84,13 +90,21 @@ endif
 	$(hide) $(call aml-copy-install-file, $(PRODUCT_OUT)/dtbo.img)
 	$(hide) $(call aml-copy-install-file, $(PRODUCT_OUT)/super_empty.img, super.img)
 	$(hide) $(call aml-copy-install-file, $(PRODUCT_OUT)/vbmeta.img)
-	$(hide) $(call aml-copy-install-file, $(PRODUCT_OUT)/misc.img)
+	$(hide) $(call aml-copy-install-file, $(FACTORY_PATH)/bootfiles/misc.img)
 	$(hide) $(AML_IMAGE_TOOL) -r  $(PRODUCT_INSTALL_OUT)/image.cfg $(PRODUCT_INSTALL_OUT)/ $@
 	$(hide) rm -rf $(PRODUCT_INSTALL_OUT)
 	$(hide) echo " $@ created"
 
 .PHONY: aml_install
 aml_install: $(INSTALLED_AML_INSTALL_PACKAGE_TARGET)
+
+BUILT_TARGET_FILES_ZIPROOT := $(call intermediates-dir-for,PACKAGING,target_files)/$(TARGET_PRODUCT)-target_files
+$(BUILT_TARGET_FILES_ZIPROOT).zip: $(BUILT_TARGET_FILES_ZIPROOT)/IMAGES/aml_install_package.img
+
+$(BUILT_TARGET_FILES_ZIPROOT)/IMAGES/aml_install_package.img: $(BUILT_TARGET_FILES_ZIPROOT).zip.list $(PRODUCT_OUT)/aml_install_package.img
+	@mkdir -p $(dir $@)
+	@cp $(PRODUCT_OUT)/aml_install_package.img $@
+	@echo $@ >> $(BUILT_TARGET_FILES_ZIPROOT).zip.list
 
 INSTALLED_RADIOIMAGE_TARGET += $(INSTALLED_AML_INSTALL_PACKAGE_TARGET)
 
@@ -120,7 +134,4 @@ endif
 .PHONY: aml_upgrade
 aml_upgrade: $(INSTALLED_AML_UPGRADE_PACKAGE_TARGET)
 
-INSTALLED_RADIOIMAGE_TARGET += $(FACTORY_PATH)/bootfiles/bootloader.img
-INSTALLED_RADIOIMAGE_TARGET += $(FACTORY_PATH)/bootfiles/bootloader-console.img
-INSTALLED_RADIOIMAGE_TARGET += $(FACTORY_PATH)/bootfiles/bootloader-recovery.img
-INSTALLED_RADIOIMAGE_TARGET += $(FACTORY_PATH)/bootfiles/misc.img
+endif
