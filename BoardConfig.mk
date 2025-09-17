fdd884639f658d8a306a6aebd1cdf98f80541f66
Bruno Martins <bgcngm@gmail.com>
2025-09-18 00:26:31 +0100

    radxa0: Switch to bcmdhd.101.10.591.x driver
    Change-Id: I0d4e6f8ef0b4a326f5c7a54c4cbeefa1b9041ed8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,7 +17,7 @@ TARGET_DTB_NAME := g12a_s905y2_radxa0
 
 ## Kernel modules
 TARGET_KERNEL_EXT_MODULES := \
-    dhd-driver/bcmdhd.101.10.361.x
+    dhd-driver/bcmdhd.101.10.591.x
 
 ## Partitions
 BOARD_SUPER_PARTITION_SIZE := 2084569088
