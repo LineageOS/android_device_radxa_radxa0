5f30223d01b61c9833b69833217f90dd1af6d631
Nolen Johnson <johnsonnolen@gmail.com>
2026-08-28 14:30:05 -0400

    radxa0: Move firmware to vendor
    * On aml_install.img flash the build/tasks guards copy the correct one.
    * On OTA, all files are copied to RADIO_FILES.
    * Mirrors existing behavior.
    
    Change-Id: I04cafa100006424a44fd741dcd31a2b27f0d31ce

--- extract-files.py
+++ extract-files.py
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+import re
+
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
@@ -12,9 +14,21 @@ from extract_utils.main import (
 module = ExtractUtilsModule(
     'radxa0',
     'radxa',
+    add_firmware_proprietary_file=True,
     skip_main_proprietary_file=True,
 )
 
 if __name__ == '__main__':
     utils = ExtractUtils.device_with_common(module, '../amlogic/g12-common', module.vendor)
     utils.run()
+
+    path = f'../../../vendor/{module.vendor}/{module.device}/Android.mk'
+    with open(path) as f:
+        content = f.read()
+    content = re.sub(
+        r'ifeq \(\$\(TARGET_DEVICE\),radxa0\)',
+        'ifneq ($(filter radxa0 radxa0_car radxa0_tab,$(TARGET_DEVICE)),)',
+        content,
+    )
+    with open(path, 'w') as f:
+        f.write(content)
