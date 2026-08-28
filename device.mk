71833664f981bed0190074acb15dc6684e9cb4c8
Nolen Johnson <johnsonnolen@gmail.com>
2026-08-28 14:30:05 -0400

    radxa0: Move firmware to vendor
    * On aml_install.img flash the build/tasks guards copy the correct one.
    * On OTA, all files are copied to RADIO_FILES.
    * Mirrors existing behavior.
    
    Change-Id: I04cafa100006424a44fd741dcd31a2b27f0d31ce

--- device.mk
+++ device.mk
@@ -35,3 +35,6 @@ include kernel/amlogic/kernel-modules/dhd-driver/firmware/wifi/wifi.mk
 
 ## Inherit from the common tree product makefile
 $(call inherit-product, device/amlogic/g12-common/g12.mk)
+
+## Inherit from the proprietary files makefile
+$(call inherit-product, vendor/radxa/radxa0/radxa0-vendor.mk)
