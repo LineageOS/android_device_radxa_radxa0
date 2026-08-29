9fbf86e0d38fea0b2e869cad2f253407484264fe
Bruno Martins <bgcngm@gmail.com>
2026-08-29 12:41:50 +0100

    radxa0: Rename and migrate init to blueprint
    Change-Id: I0013cac7636849924b0783775f22cfc379e470b3

--- device.mk
+++ device.mk
@@ -18,9 +18,9 @@ include kernel/amlogic/kernel-modules/dhd-driver/firmware/bluetooth/bluetooth.mk
 PRODUCT_HOST_PACKAGES += \
     aml_image_packer
 
-## Init-Files
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/init-files/init.amlogic.wifi_buildin.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.wifi_buildin.rc
+## Init
+PRODUCT_PACKAGES += \
+    init.amlogic.wifi_buildin.rc
 
 ## TEE
 TARGET_HAS_TEE := false
